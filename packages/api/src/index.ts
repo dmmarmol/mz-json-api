c03ad6f00019d080b7aacf8315a20d4ce993f70e
Diego Martin Marmol <diego.marmol@dataart.com>
2024-04-15 14:48:09 -0300

    feat(api): initial express server

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,24 @@
-console.log("This is the Api");
+import path from "path";
+import express from "express";
+
+const envPath = path.resolve(__dirname, "./../../../.env");
+require("dotenv").config({
+	path: envPath,
+});
+
+console.log(envPath);
+
+if (!process.env.API_PORT) {
+	throw new Error("Missing API_PORT .env variable");
+}
+
+const app = express();
+const PORT = process.env.API_PORT;
+
+app.get("/", (req, res) => {
+	res.send("API is running!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
